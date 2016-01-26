522574946b6097b3c8f6bab728f9cebe98af9e74
istateside <kevin.r.fleischman@gmail.com>
2016-01-25 22:51:14 -0500

    Finished explosions chapter. Asteroid prefab created.

--- Assets/Scripts/DestroyByCollision.cs
+++ Assets/Scripts/DestroyByCollision.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class DestroyByCollision : MonoBehaviour {
-	public GameObject explosion;
-
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
-			return;
-		}
-		Destroy (other.gameObject);
-		Destroy (gameObject);
-	}
+  public GameObject explosion;
+  public GameObject playerExplosion;
+  
+  void OnTriggerEnter(Collider other) {
+    if (other.tag == "Boundary") {
+      return;
+    }
+    
+    Instantiate(explosion, transform.position, transform.rotation);
+    if (other.tag == "Player") {
+      Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+    }
+    Destroy (other.gameObject);
+    Destroy (gameObject);
+  }
 }
