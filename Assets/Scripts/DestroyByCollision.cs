082920d22cfd9d5c7e13457778eafbb48c7dd962
istateside <kevinrfleischman@gmail.com>
2016-01-25 22:14:14 -0500

    Initial commit. On the chapter detailing explosions.

---
+++ Assets/Scripts/DestroyByCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByCollision : MonoBehaviour {
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	private Rigidbody rb;
+	public float tumble;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
